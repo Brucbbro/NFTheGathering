f98b3da4136ece3d3c036d17b383e4dae3be2d61
Brucabbro <lucaworkmail@gmail.com>
2022-05-11 10:59:40 +0200

    project setup, added required libraries and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react"
+import { AppProps } from "next/app"
+import { QueryClient, QueryClientProvider } from "react-query"
+const queryClient = new QueryClient()
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </QueryClientProvider>
+  )
 }
 
-export default MyApp
+export default App
