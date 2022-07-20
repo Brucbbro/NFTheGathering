2b535186e36b99c7cff18a735b3ff9acae2e14e3
Brucbbro <Brucbbro@users.noreply.github.com>
2022-07-20 17:39:15 +0200

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-export const GRAPHQL_ENDPOINT = "https://crossorigin.me/https://graphql.looksrare.org/graphql/"
+export const GRAPHQL_ENDPOINT = "/api/"
 export const REST_ENDPOINT = "https://api.looksrare.org/api/v1/"
 export const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 export const StrategyAnyItemFromCollectionForFixedPrice = "0x86f909f70813cdb1bc733f4d97dc6b03b8e7e8f3".toLowerCase()
